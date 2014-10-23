1d36438721a3da1ecd7057b11e0bcac767750cf2
43ndr1k <dj-blackfire@gmx.de>
2014-10-23 17:05:00 +0200

    Test in der Main

--- src/Main.java
+++ src/Main.java
@@ -11,8 +11,15 @@ public class Main {
 		spiel.setVisible(true);
 
 		Spielfeld s = new Spielfeld("410");
-		s.getAutos();
-
+		//s.getAutos();
+		
+		for(Auto a:s.getAutos()){
+			System.out.println(a.getFarbe());
+			System.out.println(a.getLaenge());
+			System.out.println(a.getRichtung());
+			System.out.println(a.getxPos());
+			System.out.println(a.getyPos());
+		}
 
 
 	}
