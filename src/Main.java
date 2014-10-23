079b68c7623280110cc8d85cc148aede2524cab8
awi2308 <infostu13@gmail.com>
2014-10-23 17:12:35 +0200

    ...

--- src/Main.java
+++ src/Main.java
@@ -9,7 +9,12 @@ public class Main {
 		//GUI wird erzeugt und angezeigt
 		RushHourGUI spiel = new RushHourGUI();
 		spiel.setVisible(true);
-		
+
+		Spielfeld s = new Spielfeld("410");
+		s.getAutos();
+
+
+
 	}
 
 }
